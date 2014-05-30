f60c06ad6d2db782fbb59246a4303cb1eb9f8e47
Richard Smith <rds45@cam.ac.uk>
2014-05-30 16:00:00 +0100

    full test coverage for scraperJSON

---
+++ test/scraperJSONSpec.js
@@ -0,0 +1,40 @@
+var sj = require('../lib/scraperJSON.js');
+var should = require('should');
+
+describe("scraperJSON", function() {
+
+  describe(".checkDefinition()", function() {
+
+    it("should reject a malformed definition", function() {
+      var defs = [{ elements: {} },
+                  { url: '.*' },
+                  {},
+                  { url: "\\.*",
+                    elements: { 'fulltext': {} }
+                  }];
+      for (var i in defs) {
+        var def = defs[i];
+        (function() {
+          sj.checkDefinition(def);
+        }).should.throwError(/^invalid ScraperJSON/);
+      }
+    });
+
+    it("should accept a well formed definition", function() {
+      var defs = [{
+        url: "\\.*",
+        elements: {
+          'fulltext': {
+            'selector': '//div'
+          }
+        }
+      }];
+      for (var i in defs) {
+        var def = defs[i];
+        sj.checkDefinition(def).should.be.ok;
+      }
+    });
+
+  });
+
+});
