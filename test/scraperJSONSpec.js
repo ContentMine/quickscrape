8ae473970e05d96074aa7c5d98f5e4465dccc84f
Richard Smith <rds45@cam.ac.uk>
2014-06-03 23:24:22 +0100

    actual full coverage of scraperJSON

--- test/scraperJSONSpec.js
+++ test/scraperJSONSpec.js
@@ -1,5 +1,11 @@
 var sj = require('../lib/scraperJSON.js');
 var should = require('should');
+log = {
+  info: function(){},
+  warn: function(){},
+  error: function(){},
+  debug: function(){}
+};
 
 describe("scraperJSON", function() {
 
@@ -41,35 +47,35 @@ describe("scraperJSON", function() {
 
   describe(".checkDefinitionUrl()", function() {
 
-    it("should reject a malformed definition", function() {
-      var defs = [{ elements: {} },
-                  { url: '.*' },
-                  { url: '.*',
-                    elements: {}
-                  },
-                  { url: "\\.*",
-                    elements: { 'fulltext': {} }
-                  }];
-      for (var i in defs) {
-        var def = defs[i];
-        (function() {
-          sj.checkDefinition(def);
-        }).should.throwError(/^invalid ScraperJSON/);
+    it("should accept a matching URL", function() {
+      var urls = [{ url: 'http://wtf.com',
+                    def: 'wtf' },
+                  { url: 'ftp://scapecity.fi',
+                    def: 'scrapecity\.fi' },
+                  { url: 'http://wtf.com',
+                    def: 'wtf' }];
+      for (var i in urls) {
+        var url = urls[i].url;
+        var def = urls[i].def;
+        sj.checkDefinitionUrl(def, url).should.be.true;
       }
     });
 
-    it("should accept a well formed definition", function() {
-      var defs = [{
-        url: "\\.*",
-        elements: {
-          'fulltext': {
-            'selector': '//div'
-          }
-        }
-      }];
-      for (var i in defs) {
-        var def = defs[i];
-        sj.checkDefinition(def).should.be.ok;
+    it("should reject a non-matching URL", function() {
+      var urls = [{ url: 'http://wtf.com',
+                    def: { url: 'http:\/\/plos.org' } },
+                  { url: 'ftp://scapecity.fi',
+                    def: { url: 'bloot' } },
+                  { url: 'http://wtf.com',
+                    def: { url: 'jp\.jp' } }];
+      for (var i in urls) {
+        var url = urls[i].url;
+        var def = urls[i].def;
+        console.log(url);
+        console.log(def);
+        var result = sj.checkDefinitionUrl(def, url);
+        console.log(result);
+        result.should.be.false;
       }
     });
 
